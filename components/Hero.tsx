1c941d997068ab8e8263a1fc682e830b7d163a90
prashant <prashant292singh@gmail.com>
2024-06-23 17:41:06 +0530

    update 4

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Prahant, a Nodejs Developer based in India.
+            Hi! I&apos;m Prashant, a Nodejs Developer based in India.
           </p>
 
           <a href="#about">
